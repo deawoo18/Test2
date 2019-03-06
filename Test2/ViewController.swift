a71f6ee89c6a60789149897f19ffd7a720f988d7
benoit <benoit.patry.1@ulaval.ca>
2019-03-06 09:19:46 -0500

    Initial Commit

---
+++ Test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test2
+//
+//  Created by Patry Benoit on 2019-03-06.
+//  Copyright © 2019 BenProg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
